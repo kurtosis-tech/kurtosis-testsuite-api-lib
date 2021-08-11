49c9d183caca8fd96c97e2d24b361f0d9e491fb3
Ali Syed <alisyed0206@gmail.com>
2021-08-11 13:09:45 -0400

    converted const to enum as needed

--- typescript/kurtosis_testsuite_docker_api/kurtosis_testsuite_docker_api.ts
+++ typescript/kurtosis_testsuite_docker_api/kurtosis_testsuite_docker_api.ts
@@ -1,8 +1,10 @@
 // TODO Rename this envvar, since the testsuite can accept whatever it wants - doesn't have to be JSON
-const CUSTOM_PARAMS_JSON_ENV_VAR = "CUSTOM_PARAMS_JSON";
-const DEBUGGER_PORT_ENV_VAR = "DEBUGGER_PORT";
-const KURTOSIS_API_SOCKET_ENV_VAR = "KURTOSIS_API_SOCKET"; // Only populated if in test-running mode
-const LOG_LEVEL_ENV_VAR = "LOG_LEVEL";
+enum KurtosisTestsuiteDockerEnvVar {
+    CustomParamsJsonEnvVar = "CUSTOM_PARAMS_JSON",
+    DebuggerPortEnvVar = "DEBUGGER_PORT",
+    KurtosisApiSocketEnvVar = "KURTOSIS_API_SOCKET", // Only populated if in test-running mode
+    LogLevelEnvVar = "LOG_LEVEL"
+}
 
 /*
 The dirpath on the testsuite container where the enclave data volume will be mounted
