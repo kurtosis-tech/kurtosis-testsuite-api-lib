160676fccd35236056ac0226acf389d2eddf631e
Ali Syed <alisyed0206@gmail.com>
2021-08-12 20:34:10 -0400

    ported test_suite_configurator into typescript

---
+++ typescript/lib/execution/test_suite_configurator.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 - present Kurtosis Technologies LLC.
+ * All Rights Reserved.
+ */
+
+import { TestSuite } from "../testsuite"; //TODO
+import { Result } from "neverthrow";
+
+// Docs available at https://docs.kurtosistech.com/kurtosis-testsuite-api-lib/lib-documentation
+interface TestSuiteConfigurator {
+	// Docs available at https://docs.kurtosistech.com/kurtosis-testsuite-api-lib/lib-documentation
+	setLogLevel(logLevelStr: string): Error;
+
+	// Docs available at https://docs.kurtosistech.com/kurtosis-testsuite-api-lib/lib-documentation
+	parseParamsAndCreateSuite(paramsJsonStr: string): Result<TestSuite, Error>
+}
